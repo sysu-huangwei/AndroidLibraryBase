c9e5d988b35c58091fe37376313daa5ddaecd5aa
黄炜 <hw1@meitu.com>
2020-04-21 16:26:07 +0800

    【Test】更新demo代码

--- app/src/main/java/com/example/androidlibrarybase/MainActivity.java
+++ app/src/main/java/com/example/androidlibrarybase/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import com.example.librarybase.LibraryBase;
 import com.example.librarybase.soloader.BaseSoLoader;
 
+import static com.example.librarybase.LibraryBase.BASE_LOG_LEVEL_ALL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         BaseSoLoader.setContext(this);
 
-        LibraryBase libraryBase = new LibraryBase();
+//        LibraryBase libraryBase = new LibraryBase();
+        LibraryBase.setLogLevel(BASE_LOG_LEVEL_ALL);
+        LibraryBase.setContext(this);
     }
 }
