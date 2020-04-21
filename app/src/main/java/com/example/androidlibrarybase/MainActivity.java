42bf44ded3d789191fb70aede1b98e6897151b23
黄炜 <hw1@meitu.com>
2020-04-21 09:58:36 +0800

    【Test】demo添加测试代码

--- app/src/main/java/com/example/androidlibrarybase/MainActivity.java
+++ app/src/main/java/com/example/androidlibrarybase/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.librarybase.LibraryBase;
+import com.example.librarybase.soloader.BaseSoLoader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BaseSoLoader.setContext(this);
+
+        LibraryBase libraryBase = new LibraryBase();
     }
 }
