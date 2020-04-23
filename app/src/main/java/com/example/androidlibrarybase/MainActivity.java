c22c1f680123b18a6765e37b7237ae7976cf7822
黄炜 <hw1@meitu.com>
2020-04-23 14:39:16 +0800

    【Feature】提交申请权限逻辑，读写权限和相机权限

--- app/src/main/java/com/example/androidlibrarybase/MainActivity.java
+++ app/src/main/java/com/example/androidlibrarybase/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidlibrarybase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.librarybase.LibraryBase;
@@ -11,15 +15,40 @@ import static com.example.librarybase.LibraryBase.BASE_LOG_LEVEL_ALL;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 动态申请定位权限的请求码
+    public static final int WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 1;
+    public static final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 2;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkAndRequestPermissions();
 
         BaseSoLoader.setContext(this);
 
 //        LibraryBase libraryBase = new LibraryBase();
         LibraryBase.setLogLevel(BASE_LOG_LEVEL_ALL);
         LibraryBase.setContext(this);
+    }
+
+    /**
+     * 动态申请权限
+     */
+    private void checkAndRequestPermissions() {
+        //写权限
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+        }
+        //读权限
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+        }
+        //相机权限
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
     }
 }
