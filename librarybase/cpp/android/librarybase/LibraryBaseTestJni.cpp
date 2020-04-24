9050f6a4c8ecec897d6ec6267bebe353bc2d96ff
黄炜 <hw1@meitu.com>
2020-04-24 15:11:31 +0800

    【Feature】添加AndroidHelper的测试方法

--- librarybase/cpp/android/librarybase/LibraryBaseTestJni.cpp
+++ librarybase/cpp/android/librarybase/LibraryBaseTestJni.cpp
@@ -4,6 +4,10 @@
 
 #include "LibraryBaseTestJni.h"
 
+#include "AndroidHelper.h"
+#include "JniHelper.h"
+#include "BaseLog.h"
+
 #define CLASS_NAME_PATH "com/example/librarybase/LibraryBaseTest"
 
 namespace librarybase {
@@ -12,6 +16,45 @@ namespace librarybase {
     public:
         static void runTest(JNIEnv *env, jclass obj) {
 
+            char *packageName = AndroidHelper::getAndroidPackageName(env, JniHelper::getContext());
+            LOGD("LibraryBaseTestJni::runTest: packageName = %s", packageName);
+            SAFE_DELETE_ARRAY(packageName);
+
+            char *androidDataDir = AndroidHelper::getAndroidDataDir(env, JniHelper::getContext());
+            LOGD("LibraryBaseTestJni::runTest: androidDataDir = %s", androidDataDir);
+            SAFE_DELETE_ARRAY(androidDataDir);
+
+            char *androidCacheDir = AndroidHelper::getAndroidCacheDir(env, JniHelper::getContext());
+            LOGD("LibraryBaseTestJni::runTest: androidCacheDir = %s", androidCacheDir);
+            SAFE_DELETE_ARRAY(androidCacheDir);
+
+            char *androidFilesDir = AndroidHelper::getAndroidFilesDir(env, JniHelper::getContext());
+            LOGD("LibraryBaseTestJni::runTest: androidFilesDir = %s", androidFilesDir);
+            SAFE_DELETE_ARRAY(androidFilesDir);
+
+            char *androidExternalCacheDir = AndroidHelper::getAndroidExternalCacheDir(env, JniHelper::getContext());
+            LOGD("LibraryBaseTestJni::runTest: androidExternalCacheDir = %s", androidExternalCacheDir);
+            SAFE_DELETE_ARRAY(androidExternalCacheDir);
+
+            char *androidExternalFilesDir = AndroidHelper::getAndroidExternalFilesDir(env, JniHelper::getContext());
+            LOGD("LibraryBaseTestJni::runTest: androidExternalFilesDir = %s", androidExternalFilesDir);
+            SAFE_DELETE_ARRAY(androidExternalFilesDir);
+
+            char *androidExternalStorageDirectory = AndroidHelper::getAndroidExternalStorageDirectory(env);
+            LOGD("LibraryBaseTestJni::runTest: androidExternalStorageDirectory = %s", androidExternalStorageDirectory);
+            SAFE_DELETE_ARRAY(androidExternalStorageDirectory);
+
+            int androidCpuCount = AndroidHelper::getAndroidCpuCount();
+            LOGD("LibraryBaseTestJni::runTest: androidCpuCount = %d", androidCpuCount);
+
+            int androidSDKVersion = AndroidHelper::getAndroidSDKVersion(env);
+            LOGD("LibraryBaseTestJni::runTest: androidSDKVersion = %d", androidSDKVersion);
+
+            bool mkDirsSuccess = AndroidHelper::mkDirs(env, "/mnt/sdcard/Android/data/com.example.androidlibrarybase/files/librarybase");
+            LOGD("LibraryBaseTestJni::runTest: mkDirs = %s mkDirsSuccess = %d", "mnt/sdcard/librarybase", mkDirsSuccess);
+
+            bool deleteFileSuccess = AndroidHelper::deleteFile(env, "/mnt/sdcard/Android/data/com.example.androidlibrarybase/files/librarybase");
+            LOGD("LibraryBaseTestJni::runTest: deleteFile = %s deleteFileSuccess = %d", "mnt/sdcard/librarybase", deleteFileSuccess);
         }
     };
 
