2b59745476a384c36b20cd8b7c336f30f0e13248
黄炜 <hw1@meitu.com>
2020-04-30 14:01:56 +0800

    【Test】提交demo

--- app/src/main/java/com/example/androidlibrarybase/MainActivity.java
+++ app/src/main/java/com/example/androidlibrarybase/MainActivity.java
@@ -9,16 +9,25 @@ import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 import com.example.librarybase.LibraryBase;
 import com.example.librarybase.LibraryBaseTest;
-import com.example.librarybase.nativebitmap.NativeBitmap;
+import com.example.librarybase.opengl.Base2DTexturePainter;
+import com.example.librarybase.opengl.BaseGLUtils;
 import com.example.librarybase.soloader.BaseSoLoader;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.FloatBuffer;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
 import static com.example.librarybase.LibraryBase.BASE_LOG_LEVEL_ALL;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
     public static final int WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 1;
     public static final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 2;
     public static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
+
+    Base2DTexturePainter mBase2DTexturePainter = new Base2DTexturePainter();
+
+    private Bitmap mBitmap = null;
+    private int mTexture = 0;
+
+    private GLSurfaceView mGLSurfaceView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +66,38 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-        NativeBitmap nativeBitmap = new NativeBitmap();
-        nativeBitmap.setBitmap(bitmap);
-        Bitmap bitmapCopy = nativeBitmap.toBitmap();
+
+        mBitmap = BitmapFactory.decodeStream(inputStream);
 
         LibraryBaseTest libraryBaseTest = new LibraryBaseTest();
         libraryBaseTest.runTest();
+
+        mGLSurfaceView = findViewById(R.id.gl_surface_view);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+
+        mGLSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+                mTexture = BaseGLUtils.createTextures2DWithBitmap(mBitmap, GLES20.GL_RGBA);
+                mBase2DTexturePainter.init();
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+                mBase2DTexturePainter.viewPort(0, 0, width, height);
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+                mBase2DTexturePainter.render(mTexture, mBitmap.getWidth(), mBitmap.getHeight());
+            }
+        });
+
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        mGLSurfaceView.requestRender();
+
+
     }
 
     /**
